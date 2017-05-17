377c2509b51070d1a7d26640bc5d3b807550452b
Erik Tauchert <erik@tauchert.de>
2017-05-17 09:35:01 +0200

    add reading attributes and instances

--- Car_ID3/Program.cs
+++ Car_ID3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,56 @@ namespace Car_ID3
 	{
 		static void Main(string[] args)
 		{
+			var metadata = ReadMetaData("car_data\\car.c45-names");
+			var instances = ReadInstances(metadata, "car_data\\car.data");
+		}
+
+		static (string[] classValues, TrainAttribute[] attributes) ReadMetaData(string filename)
+		{
+			using (var file = File.OpenRead(filename))
+			using (var reader = new StreamReader(file))
+			{
+				var classValues = new string[0];
+				var attributes = new List<TrainAttribute>();
+				while (!reader.EndOfStream)
+				{
+					var line = reader.ReadLine();
+					if (line.StartsWith("| class values"))
+					{
+						while (reader.Peek() != '|' && !reader.EndOfStream)
+						{
+							line = reader.ReadLine().Trim();
+							if (line.Length > 0) { classValues = line.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray(); }
+						}
+					}
+					else if (line.StartsWith("| attributes"))
+					{
+						while (reader.Peek() != '|' && !reader.EndOfStream)
+						{
+							var attr = TrainAttribute.ReadFromStream(reader);
+							if (attr != null) attributes.Add(attr);
+						}
+					}
+				}
+				return (classValues, attributes.ToArray());
+			}
+		}
+
+		static int[][] ReadInstances((string[] classValues, TrainAttribute[] attributes) metadata, string filename)
+		{
+			var valueCount = metadata.attributes.Length + 1;
+			using (var file = File.OpenRead(filename))
+			using (var reader = new StreamReader(file))
+			{
+				var instances = new List<int[]>();
+				while (!reader.EndOfStream)
+				{
+					var values = reader.ReadLine().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+					if (values.Length != valueCount) continue;
+					instances.Add(Enumerable.Range(0, valueCount).Select(i => Array.IndexOf((i < valueCount - 1 ? metadata.attributes[i].PossibleValues : metadata.classValues), values[i])).ToArray());
+				}
+				return instances.ToArray();
+			}
 		}
 	}
 }
---
+++ Car_ID3/TrainAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_ID3
+{
+	class TrainAttribute
+	{
+		public string Name { get; private set; }
+		public string[] PossibleValues { get; private set; }
+
+		public TrainAttribute(string name, string[] possibleValues)
+		{
+			Name = name;
+			PossibleValues = possibleValues;
+		}
+
+		public static TrainAttribute ReadFromStream(StreamReader reader)
+		{
+			var line = reader.ReadLine();
+			int idx;
+			if ((idx = line.IndexOf(':')) == -1) return null;
+			var name = line.Substring(0, idx);
+			line = line.Substring(idx + 1);
+			if ((idx = line.IndexOf('.')) == -1) return null;
+			var values = line.Substring(0, idx).Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+			return new TrainAttribute(name, values);
+		}
+	}
+}
