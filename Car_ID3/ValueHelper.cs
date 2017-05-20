8d411eac1587ebd06cae2395d89a9c687764266b
Erik Tauchert <erik@tauchert.de>
2017-05-20 18:00:41 +0200

    move converter generation into non-generic class

--- Car_ID3/ValueHelper.cs
+++ Car_ID3/ValueHelper.cs
@@ -9,10 +9,15 @@ namespace Car_ID3
 {
 	class ValueHelper<T> where T : struct
 	{
-		public static readonly Func<int, T> Convert = GenerateConverter<int, T>();
-		public static readonly Func<T, int> ConvertBack = GenerateConverter<T, int>();
+		public static readonly Func<int, T> Convert = ValueHelper.GenerateConverter<int, T>();
+		public static readonly Func<T, int> ConvertBack = ValueHelper.GenerateConverter<T, int>();
+	}
 
-		static Func<Tin, Tout> GenerateConverter<Tin, Tout>()
+	class ValueHelper
+	{
+		public static T Convert<T>(int value) where T : struct => ValueHelper<T>.Convert(value);
+
+		public static Func<Tin, Tout> GenerateConverter<Tin, Tout>()
 		{
 			var parameter = Expression.Parameter(typeof(Tin));
 			var method = Expression.Lambda<Func<Tin, Tout>>(
@@ -20,10 +25,5 @@ namespace Car_ID3
 				parameter);
 			return method.Compile();
 		}
-	}
-
-	class ValueHelper
-	{
-		public static T Convert<T>(int value) where T : struct => ValueHelper<T>.Convert(value);
 	}
 }
